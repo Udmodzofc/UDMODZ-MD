f0f82298a2ecc535a75c129bb4e68c3fab3fb480
UDhanika Dissanayaka <udmodzofc@gmail.com>
2025-04-17 21:17:47 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "සෙසන් දාන්න", // session id
+SESSION_ID: process.env.SESSION_ID || "සෙසන්", // session id
 ALIVE_IMG : process.env.ALIVE_IMG || "https://i.ibb.co/5xzWmSxn/20250224-094453.png",
 ALIVE_MSG : process.env.ALIVE_MSG || "*🤖𝐇𝐞𝐲",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true", // true,false
 ANTI_DELETE: process.env.ANTI_DELETE || "true", // true,false
 MODE: process.env.MODE || "public", // public,private,groups,inbox
-AUTO_READ_STATUS: process.env.ALLWAYS_OFFLINE || "false", // true,false
+ALLWAYS_OFFLINE: process.env.ALLWAYS_OFFLINE || "false", // true,false
 LANG: process.env.LANG || "EN",
 
 
